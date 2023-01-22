3ab91b7d5fe330403576a07345dc26dc3183ef02
Erdem Dirican <erdemdirican@hotmail.com>
2023-01-22 14:42:00 +0300

    Add project files.

---
+++ Logging/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Logging.Controllers
+{
+    [Route("api/home")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var zero = 0;
+            var result = 50 / zero;
+            return Ok(result);
+        }
+    }
+}
---
+++ Logging/Filters/ExceptionFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Logging.Filters
+{
+    public class ExceptionFilter : ExceptionFilterAttribute
+    {
+        private readonly ILogger<ExceptionFilter> _logger;
+
+        public ExceptionFilter(ILogger<ExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public override void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception.Message, context.Exception);
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Result = new JsonResult("A system error has occurred.");
+        }
+    }
+}
